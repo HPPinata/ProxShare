d9a25ead7b6d247510a4055b39b80e17f0c9a99a
HPPinata <83947761+HPPinata@users.noreply.github.com>
2023-08-18 20:46:06 +0200

    use lvm-cache

--- createSHR.bash
+++ createSHR.bash
@@ -37,33 +37,34 @@ create-TEMPLATE () {
 create-TEMPLATE
 
 apt update && apt full-upgrade -y && apt autopurge -y
-apt install -y bcache-tools duperemove samba snapper
+apt install -y duperemove samba snapper
 
 systemctl enable smb
 
-modprobe bcache
-echo 1 | tee /sys/fs/bcache/*/stop
-echo 1 | tee /sys/block/bcache*/bcache/stop
-sleep 1
-
+vgchange -an data
 pass=$(ls /dev/sd*)
 wipefs -f -a ${pass[@]} /dev/nvme0n1
 
-make-bcache -C /dev/nvme0n1
-make-bcache -B ${pass[@]}
-sleep 1
+pvcreate -ff -y /dev/nvme0n1
+pvcreate -ff -y ${pass[@]}
 
-bcache-super-show /dev/nvme0n1 | grep cset.uuid | awk -F ' ' {'print $2'} | tee /sys/block/bcache*/bcache/attach
-echo writeback | tee /sys/block/bcache*/bcache/cache_mode
-echo 0 | tee /sys/block/bcache*/bcache/writeback_percent
+vgcreate data /dev/nvme0n1 ${pass[@]}
 
-wipefs -f -a $(find /dev/bcache* -maxdepth 0 -type b)
-mkfs.btrfs -f -L data -m raid1 -d raid1 $(find /dev/bcache* -maxdepth 0 -type b)
+for i in ${pass[@]}; do lvcreate -n $(basename $i) -l 100%PV data $i; done
+x=$(pvdisplay /dev/nvme0n1 | grep "Free PE" | awk -F ' ' {'print $3'})
+n=$(( $(lvs data | wc -l) - 1 ))
+for (( i=1; i<=$n; i++ )) do lvcreate -n cache$i -l $(( $x / $n )) data /dev/nvme0n1; done
+
+c=1
+for i in $(lvs data | grep sd | awk -F ' ' {'print $1'}); do lvconvert -y --type cache --cachevol cache$c data/$i; let c++; done
+
+wipefs -f -a $(ls /dev/data)
+mkfs.btrfs -f -L data -m raid1 -d raid1 $(ls /dev/data)
 
 mkdir -p /var/share/mnt
-mount /dev/bcache0 /var/share/mnt
+mount /dev/data/sda /var/share/mnt
 
-{ echo; echo '/dev/bcache0  /var/share/mnt  btrfs  nofail  0  2'; } >> /etc/fstab
+{ echo; echo '/dev/data/sda  /var/share/mnt  btrfs  nofail  0  2'; } >> /etc/fstab
 
 fstrim -av
 
@@ -96,8 +97,6 @@ snapper -c data setup-quota
 { crontab -l 2>/dev/null
 cat <<'EOL'
 
-#@reboot echo 0 | tee /sys/block/bcache*/bcache/sequential_cutoff
-@reboot echo 0 | tee /sys/block/bcache*/bcache/writeback_percent
 0 6 * * 1 duperemove -dhr --dedupe-options=same --hash=xxhash --hashfile=/var/share/mnt/.duperemove/hashfile.db /var/share/mnt
 0 5 1 * * rm -rf /var/share/mnt/.duperemove/hashfile.db && btrfs filesystem defragment -r /var/share/mnt
 0 5 20 * * btrfs scrub start /var/share/mnt
